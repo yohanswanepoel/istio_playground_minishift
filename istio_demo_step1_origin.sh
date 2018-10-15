31e84f3753febfa1170488468e645bd95346a823
Johan Swanepoel <johannes.swanepoel@gmail.com>
2018-10-15 11:18:57 +1100

    Added step 1 for origin

---
+++ istio_demo_step1_origin.sh
@@ -0,0 +1,9 @@
+minishift profile delete servicemesh
+minishift profile set servicemesh
+minishift config set memory 10GB
+minishift config set cpus 4
+minishift config set image-caching true
+minishift config set openshift-version v3.11.0
+minishift addon enable admin-user
+minishift addon enable anyuid
+minishift start
