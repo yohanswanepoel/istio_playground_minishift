30265eb1f30cd826a3a39fe48e2595936d09353d
yohanswanepoel <johannes.swanepoel@gmail.com>
2018-10-15 11:28:11 +1100

    Update istio_demo_step1.sh

--- istio_demo_step1.sh
+++ istio_demo_step1.sh
@@ -3,7 +3,7 @@ minishift profile set servicemesh
 minishift config set memory 10GB
 minishift config set cpus 4
 minishift config set image-caching true
-minishift config set ocp-tag v3.11.16
+minishift config set ocp-tag v3.10.45
 minishift addon enable admin-user
 minishift addon enable anyuid
 minishift start
