487416e7f1c49cf6603130f10b45b76a096c97d0
Johan Swanepoel <johannes.swanepoel@gmail.com>
2018-10-23 10:27:47 +1100

    Fixed mtls v non mtls config

--- step1_cdk.sh
+++ step1_cdk.sh
@@ -1,7 +1,7 @@
 minishift stop
 minishift profile delete servicemesh
 minishift profile set servicemesh
-minishift config set memory 10GB
+minishift config set memory 12GB
 minishift config set cpus 4
 minishift config set image-caching true
 minishift config set ocp-tag v3.10.45
